a1f096eb19ce8f0e156d97817268e752c0bfc734
Titho93 <titho10.93@gmail.com>
2023-09-19 12:20:30 +0200

    [init] new reoprt old file

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// 1. seleziono il container e lo salvo in una variabile
+const container   = document.querySelector('.container');
+// 2. Creo un bottone 
+const startButton = document.getElementById("start");
+reset();
+
+
+// 3. faccio un ciclo su 100
+for(let i = 1; i <= 100; i++){
+    // 4.2. ad ogni ciclo devo far generare un quadrato
+    const square = addSquare(i);
+  
+  // 6. per ogni quadrato aggiungere un evento al click
+// 7. al click del quadrato aggiungere/togliere(toggle) la classe del click
+  square.addEventListener('click', function(){
+      console.log(this._squareId)
+  
+      this.classList.toggle('clicked');
+    })
+  
+    container.append(square)
+  }
+  
+  // 4.1. creo una funzione per generare quadrati 
+  function addSquare(index){
+  
+    const newSquare = document.createElement('div');
+    newSquare.className = 'square';
+    newSquare._squareId = index;
+    newSquare.innerHTML = `<span>${index}</span>`
+    return newSquare;
+  
+  }
+  
+  // 5. resetto i quadrati nell'html 
+  function reset(){
+    container.innerHTML = '';
+  }
+  
+  // 8. imposto una funzione in modo che al click del bottone venga generata la griglia
+  startButton.addEventListener('click', function(){
+    container.classList.add('none');
+  })
+  
+  
+  
